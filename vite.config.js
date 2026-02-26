02854f7691170d26c60b3facf9ae4f07bbc2610a
tejesh-lab <pathitejesh@gmail.com>
2026-02-26 12:09:51 +0530

    changes in config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //  base: "/ecomm-app/"
-  
+  base: process.env.VERCEL ? "/" : "/ecomm-app/"
 })
